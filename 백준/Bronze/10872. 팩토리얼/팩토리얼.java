d0319a3c5d08fc31f573f5b343ed61378fd8e230
Park Soomin <soomini030113@gmail.com>
2026-03-04 20:02:26 +0900

    [Bronze III] Title: 팩토리얼, Time: 172 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int mul = 1;
+
+        for (int i=1; i<=n; i++) {
+            mul *= i;
+        }
+
+        System.out.println(mul);
+    }
+}
