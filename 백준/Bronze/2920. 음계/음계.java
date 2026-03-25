f0a1c0e83e086a73dd3770f910d267e086d9c809
Park Soomin <soomini030113@gmail.com>
2026-03-25 16:02:16 +0900

    [Bronze II] Title: 음계, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = bufferedReader.readLine();
+
+        if (input.equals("1 2 3 4 5 6 7 8")) {
+            System.out.println("ascending");
+        } else if (input.equals("8 7 6 5 4 3 2 1")) {
+            System.out.println("descending");
+        } else {
+            System.out.println("mixed");
+        }
+    }
+}
