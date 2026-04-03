8fcfd4c6c3448f62195a9d92e7e122cd8384ac32
Park Soomin <soomini030113@gmail.com>
2026-04-03 14:03:17 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 96 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("       _.-;;-._\n").append("'-..-'|   ||   |\n").append("'-..-'|_.-;;-._|\n")
+                        .append("'-..-'|   ||   |\n").append("'-..-'|_.-''-._|");
+
+        System.out.println(stringBuilder);
+    }
+}
