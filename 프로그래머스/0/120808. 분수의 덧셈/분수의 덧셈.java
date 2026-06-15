48b5633f4830908a885c188717733412fa952a8a
Park Soomin <soomini030113@gmail.com>
2026-06-15 22:21:57 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.02 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,36 +1,16 @@
 class Solution {
+    public int getGCD(int a, int b) {
+        if (a % b == 0) return b;
+        return getGCD(b, a%b);
+    }
+    
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
-        int denom = 0;
+        int denom = denom1 * denom2;
+        int numer = numer1 * denom2 + numer2 * denom1;
         
-        if (denom1 == denom2) {
-            denom = denom1;
-        } else {
-            denom = denom1 * denom2;
-            numer1 *= denom / denom1;
-            numer2 *= denom / denom2;
-        }
-        int numer = numer1 + numer2;
+        int gcd = getGCD(denom, numer);
         
-        int a,b = 0;
-        if (numer >= denom) {
-            a = numer;
-            b = denom;
-        } else {
-            a = denom;
-            b = numer;
-        }
-        
-        while(true) {
-            if (a%b == 0) {
-                break;
-            } else {
-                int temp = a;
-                a = b;
-                b = temp%b;
-            }
-        }
-        
-        int[] answer = {numer/b, denom/b};
+        int[] answer = {numer/gcd, denom/gcd};
         
         return answer;
     }
