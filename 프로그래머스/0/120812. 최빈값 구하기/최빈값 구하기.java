88eab09545ed8da9411935341b17d77d8c6e1670
Park Soomin <soomini030113@gmail.com>
2026-06-16 20:28:37 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] array) {
+        
+        int[] count = new int[1000];
+        for (int i=0; i<array.length; i++) {
+            count[array[i]]++;
+        }
+        
+        int max = count[0];
+        int answer = 0;
+        for (int i=0; i<count.length; i++) {
+            if (max < count[i]) {
+                max = count[i];
+                answer = i;
+            }
+        }
+        
+        int maxCount = 0;
+        for (int i=0; i<count.length; i++) {
+            if (max == count[i]) {
+                maxCount++;
+            }
+        }
+        
+        if (maxCount > 1) answer = -1;
+        
+        return answer;
+    }
+}
