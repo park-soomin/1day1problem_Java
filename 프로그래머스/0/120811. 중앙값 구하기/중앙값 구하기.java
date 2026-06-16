cbb76c9bbf61eb60effcd3a8c48c6dce996e6ccd
Park Soomin <soomini030113@gmail.com>
2026-06-16 13:32:44 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -1,8 +1,39 @@
-import java.util.Arrays;
-
 class Solution {
+    public int partition(int[] array, int left, int right) {
+        int pivot = array[left];
+        int i = left;
+        int j = right + 1;
+        int temp;
+        
+        do {
+            do {i++;} while (i < right && pivot >= array[i]);
+            do {j--;} while (j > left && pivot <= array[j]);
+            if (i < j) {
+                temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        } while (i < j);
+        
+        temp = array[left];
+        array[left] = array[j];
+        array[j] = temp;
+        
+        return j;
+    }
+    
+    public void quickSort(int[] array, int left, int right) {
+        if (left < right) {
+            int pivot = partition(array, left, right);
+            
+            quickSort(array, left, pivot-1);
+            quickSort(array, pivot+1, right);
+        }
+    }
+    
     public int solution(int[] array) {
-        Arrays.sort(array);
+        quickSort(array, 0, array.length-1);
+        
         return array[array.length/2];
     }
 }
