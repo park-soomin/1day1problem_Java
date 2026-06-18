a2e620f5a3cac18aad54626c6644ee74600b8f99
Park Soomin <soomini030113@gmail.com>
2026-06-18 14:01:10 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.04 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.java
@@ -1,12 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuffer sb = new StringBuffer();
-        
-        for(int i=0; i<my_string.length(); i++) {
-            sb.append(my_string.charAt(my_string.length() - i - 1));
-        }
-        
-        String answer = sb.toString();
-        return answer;
+        StringBuilder sb = new StringBuilder(my_string);
+        return sb.reverse().toString();
     }
 }
