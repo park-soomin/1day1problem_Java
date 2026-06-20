faaa62d1432b7205f5bb0cbf340a3b2b8f9e011b
Park Soomin <soomini030113@gmail.com>
2026-06-20 13:14:11 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -3,31 +3,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         
         while (age > 0) {
-            int n = age % 10;
+            sb.insert(0, (char)(age%10 + (int)'a'));
             age /= 10;
-            
-            switch (n) {
-                case 0: sb.insert(0, "a");
-                    break;
-                case 1: sb.insert(0, "b");
-                    break;
-                case 2: sb.insert(0, "c");
-                    break;
-                case 3: sb.insert(0, "d");
-                    break;
-                case 4: sb.insert(0, "e");
-                    break;
-                case 5: sb.insert(0, "f");
-                    break;
-                case 6: sb.insert(0, "g");
-                    break;
-                case 7: sb.insert(0, "h");
-                    break;
-                case 8: sb.insert(0, "i");
-                    break;
-                case 9: sb.insert(0, "j");
-                    break;
-            }
         }
 
         return sb.toString();
