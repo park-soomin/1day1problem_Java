a862864c8e435c0296bcbdbf9220e4a71b42dc69
Park Soomin <soomini030113@gmail.com>
2026-06-22 23:51:02 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.06 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,8 +1,11 @@
 class Solution {
-    public int binom(int n, int k) {
-        if (k == 0) return 1;
-        if (k == n) return 1;
-        return binom(n-1, k) + binom(n-1, k-1);
+    int[][] memo = new int[31][31];
+
+    int binom(int n, int k) {
+        if (k == 0 || k == n) return 1;
+        if (memo[n][k] != 0) return memo[n][k];
+        memo[n][k] = binom(n-1, k) + binom(n-1, k-1);
+        return memo[n][k];
     }
     
     
