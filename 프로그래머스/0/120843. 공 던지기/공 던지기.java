119b9440b235f5b59c9298260e2ca92ded6c6501
Park Soomin <soomini030113@gmail.com>
2026-06-23 15:20:04 +0900

    [level 0] Title: 공 던지기, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -1,14 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        int i = 0;
-        for (int j=0; j<k; j++) {
-            if (i >= numbers.length) {
-                i -= numbers.length;
-            }
-            answer = numbers[i];
-            i += 2;
-        }
-        return answer;
+        return (k-1)*2 % numbers.length + 1;
     }
 }
