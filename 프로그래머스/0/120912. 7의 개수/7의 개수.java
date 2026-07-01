fcea2ccd8cc85392e29ae5afdfad437e7fb195d6
Park Soomin <soomini030113@gmail.com>
2026-07-01 17:19:39 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 88.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] array) {
+        
+        int answer = 0;
+        
+        for (int num : array) {
+            while (num > 0) {
+                if (num % 10 == 7) {
+                    answer++;
+                }
+                num /= 10;
+            }
+        }
+        
+        return answer;
+    }
+}
