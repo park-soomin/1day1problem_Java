ceb49b5824a51fd5c1e051e6fadbb4398f970bc7
Park Soomin <soomini030113@gmail.com>
2026-07-08 19:40:42 +0900

    [level 0] Title: 특이한 정렬, Time: 6.60 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        
+        Integer[] arr = Arrays.stream(numlist)
+                            .boxed()
+                            .toArray(Integer[]::new);
+        
+        Arrays.sort(arr, (a,b) -> {
+            int distA = Math.abs(a - n);
+            int distB = Math.abs(b - n);
+            
+            if (distA != distB) return distA - distB;
+            return b - a;
+        });
+        
+        return Arrays.stream(arr)
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+    }
+}
