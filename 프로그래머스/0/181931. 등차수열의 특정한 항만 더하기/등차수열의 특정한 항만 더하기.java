13748cdb0812644cb6c748d1fc7040948528a015
Park Soomin <soomini030113@gmail.com>
2026-09-03 16:07:50 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -3,10 +3,9 @@ class Solution {
         int answer = 0;
         
         for (int i=0; i<included.length; i++) {
-            if (included[i] == false) {
-                continue;
+            if (included[i]) {
+                answer += a + i * d;
             }
-            answer += a + i * d;
         }
         
         return answer;
