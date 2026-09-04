cde00374aade266b854f96f797734f206e57227a
Park Soomin <soomini030113@gmail.com>
2026-09-04 14:25:03 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.09 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,20 +1,18 @@
 class Solution {
     public int solution(int n, String control) {
         
-        int answer = n;
-        
         for (int i=0; i<control.length(); i++) {
             if (control.charAt(i) == 'w') {
-                answer += 1;
+                n += 1;
             } else if (control.charAt(i) == 's') {
-                answer -= 1;
+                n -= 1;
             } else if (control.charAt(i) == 'd') {
-                answer += 10;
+                n += 10;
             } else if (control.charAt(i) == 'a') {
-                answer -= 10;
+                n -= 10;
             }
         }
         
-        return answer;
+        return n;
     }
 }
