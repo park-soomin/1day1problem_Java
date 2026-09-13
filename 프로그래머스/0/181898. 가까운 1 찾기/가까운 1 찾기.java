f7423e41bbfabc2721ff8126b11b75a200414b7c
Park Soomin <soomini030113@gmail.com>
2026-09-13 15:39:21 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -5,8 +5,7 @@ class Solution {
         
         for (int i=idx; i<arr.length; i++) {
             if (arr[i] == 1) {
-                answer = i;
-                break;
+                return i;
             }
         }
         
