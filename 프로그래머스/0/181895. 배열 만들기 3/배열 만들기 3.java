b4f86a05aeec63b80185bc3dfdbf9b4d82083d4d
Park Soomin <soomini030113@gmail.com>
2026-09-13 16:18:13 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.14 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        
+        int size = (intervals[0][1] - intervals[0][0]) +
+            (intervals[1][1] - intervals[1][0]) + 2;
+        int[] answer = new int[size];
+        
+        System.arraycopy(arr,intervals[0][0],
+                         answer,0,intervals[0][1]-intervals[0][0]+1);
+        System.arraycopy(arr,intervals[1][0],
+                         answer,intervals[0][1]-intervals[0][0]+1,
+                         intervals[1][1]-intervals[1][0]+1);
+        
+        return answer;
+    }
+}
