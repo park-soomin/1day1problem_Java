1ab0e1f7ce9d56ebe435572eca7a85edfd90f269
Park Soomin <soomini030113@gmail.com>
2026-09-15 19:16:41 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        
+        int answer = 0;
+        
+        for (int num : numbers) {
+            if (answer > n) break;
+            answer += num;
+        }
+        
+        return answer;
+    }
+}
