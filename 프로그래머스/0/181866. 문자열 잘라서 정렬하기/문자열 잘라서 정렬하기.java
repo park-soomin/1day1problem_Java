c7cccd34990d555458681a668616ad87a974e9f6
Park Soomin <soomini030113@gmail.com>
2026-09-22 19:02:04 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 18.72 ms, Memory: 91.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String myString) {
+        
+        String[] answer = Arrays.stream(myString.split("x"))
+                                .filter(s -> !s.isEmpty())
+                                .sorted()
+                                .toArray(String[]::new);
+        
+        return answer;
+    }
+}
