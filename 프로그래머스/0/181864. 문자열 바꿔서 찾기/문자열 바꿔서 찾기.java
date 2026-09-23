8f7e80dfa745c7cabe85ca8d076bdb4777a1e4e5
Park Soomin <soomini030113@gmail.com>
2026-09-23 13:16:45 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.04 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(String myString, String pat) {
 
-        myString = myString.replace("A", "b");
-        myString = myString.replace("B", "a");
-        myString = myString.toUpperCase();
-        
-        if (myString.contains(pat)) {
-            return 1;
-        }
-        
-        return 0;
+        return myString.replace("A", "b")
+                    .replace("B", "a")
+                    .toUpperCase()
+                    .contains(pat) ? 1 : 0;
     }
 }
